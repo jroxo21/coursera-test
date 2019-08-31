6e8c29ed8ff5db558717cc49ce55ea12dd236fe6
jonatassilva <jroxo21@hotmail.com>
2019-08-31 10:19:06 +0000

    week 1 done :)

--- ExpressionsAndInterpolation/app.js
+++ ExpressionsAndInterpolation/app.js
@@ -7,6 +7,12 @@
     MsgController.inject['$scope'];
     function MsgController($scope) {
         $scope.name="Jonatas";
+        $scope.stateOfBeing="hungry";
+
+        $scope.feedJonatas = function () {
+            $scope.stateOfBeing = "wellFed"
+        }
+
         $scope.sayHello = function (){
             return 'Hello my friend!'
         }
